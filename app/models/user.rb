163beb41034ad64a86d297462462aeb193a5b9f6
Thomasl <thomas.levot@lamache.org>
2017-11-15 20:44:33 +0000

    Table User OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+         validates :civility, presence:true, length: {maximum: 10}
+         validates :first_name, presence:true, length: {maximum: 50}
+         validates :last_name, presence:true, length: {maximum: 50}
+         validates :social_reason, presence:true, length: {maximum: 100}
+         validates :profil, presence:true, length: {maximum: 100}
+         validates :siret, presence:true, length: {maximum: 14}
+         validates :tel, presence:true, length: {maximum: 12}
+         validates :rubric, presence:true, length: {maximum: 50}
+         validates :address, presence:true, length: {maximum: 150}
+         validates :city, presence:true, length: {maximum: 60}
+         
+         
+         
 end
---
+++ db/migrate/20171115181346_add_field_to_user.rb
@@ -0,0 +1,14 @@
+class AddFieldToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :civility, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :social_reason, :string
+    add_column :users, :profil, :string
+    add_column :users, :siret, :integer
+    add_column :users, :tel, :integer
+    add_column :users, :rubric, :string
+    add_column :users, :address, :string
+    add_column :users, :city, :string
+  end
+end
