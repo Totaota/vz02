99427d7d786a0cdfc6ec0c01866c8ed677566608
Thomasl <thomas.levot@lamache.org>
2017-11-16 23:15:09 +0000

    Création controller Room

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,43 @@
+class CompanyController < ApplicationController
+    
+before_action :set_company, only: [:show, :edit, :update]
+before_action :authenticate_user!, except: [:show]
+    
+    def index
+       @companies = current_user.companies 
+    end
+    
+    def new
+        @company = current_user.companies.build(company_params)
+    end
+    
+    def create
+            @company = current_user.companies.build(company_params)
+            if @company.save
+                redirect_to @company, notice:"Votre entreprise a été ajoutée avec succès"
+            else
+                render :new
+            end
+    end
+    
+    def update
+       if @company.update(company_params)
+              redirect to @company, notice:"Modification enregistrée..."
+       else
+           render :edit
+       end
+    end
+    
+private
+ 
+    def set_company
+         @company = Company.find(params[:id])
+    end
+        
+    def company_params
+        params.require(:company).permit(:social_reason, :email_company, :address_company,:tel_company, :legal_status,
+                                        :num_tva, :rcs, :ape_code, :description, :offer, :product, :creation_date, :website,
+                                        :staff, :turnover)
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   
   
   resources :users, only: [:show]
+  resources :companies
 
   
   # The priority is based upon order of creation: first created -> highest priority.
